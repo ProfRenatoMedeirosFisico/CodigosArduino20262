ee30127e23457bf007564b93bf6c3b15a2de54cc
ProfRenatoMedeirosFisico <renato.medeiros@ueg.br>
2026-09-11 10:38:59 -0300

    Update 07_06_multiplos_modos_switch_case.ino
    updated

--- Capitulo_07/07_06_multiplos_modos_switch_case.ino
+++ Capitulo_07/07_06_multiplos_modos_switch_case.ino
@@ -1,6 +1,11 @@
 int leds[] = {2, 3, 4, 5, 6, 7};
 int numLeds = 6;
-int modo = 2;
+
+int botao1 = 8;   // pressionar -> modo 1 (piscarTodos)
+int botao2 = 9;   // pressionar -> modo 2 (knightRider)
+int botao3 = 10;  // pressionar -> modo 3 (sequencial)
+
+int modo = 2;     // modo inicial, antes de qualquer botão ser pressionado
 
 void todosDesligados() {
   for (int i = 0; i < numLeds; i++) {
@@ -31,7 +36,6 @@ void knightRider() {
     digitalWrite(leds[i], HIGH);
     delay(80);
   }
-
   for (int i = numLeds - 2; i >= 1; i--) {
     todosDesligados();
     digitalWrite(leds[i], HIGH);
@@ -39,13 +43,25 @@ void knightRider() {
   }
 }
 
+void lerBotoes() {
+  // INPUT_PULLUP: pino fica HIGH em repouso e cai para LOW quando o botão é pressionado
+  if (digitalRead(botao1) == LOW) modo = 1;
+  if (digitalRead(botao2) == LOW) modo = 2;
+  if (digitalRead(botao3) == LOW) modo = 3;
+}
+
 void setup() {
   for (int i = 0; i < numLeds; i++) {
     pinMode(leds[i], OUTPUT);
   }
+  pinMode(botao1, INPUT_PULLUP);
+  pinMode(botao2, INPUT_PULLUP);
+  pinMode(botao3, INPUT_PULLUP);
 }
 
 void loop() {
+  lerBotoes();
+
   switch (modo) {
     case 1:
       piscarTodos();
